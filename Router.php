e53dd0d87908744dbb4498c28313b4047ab2a8d9
IgorCN <icnigor1512@gmail.com>
2023-12-18 21:53:10 -0300

    comentario adicionado na classe Router [Versão: 0.3.2]

--- Router.php
+++ Router.php
@@ -6,7 +6,7 @@ class Router extends RouteSwitch
 {
    public function run(string $requestUri)
    {
-      $route = substr($requestUri, 1);
+      $route = substr($requestUri, 1); // viola o SRP (Single Responsability Principle) - S do S.O.L.I.D.
 
       if ($route === '') {
          $this->home();
